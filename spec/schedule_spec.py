0790f4ba103e9acf0632f21178c57efb72303c47
Edderic Ugaddan <edderic@gmail.com>
2016-09-15 15:10:04 -0400

    add test for schedule_spec

--- spec/schedule_spec.py
+++ spec/schedule_spec.py
@@ -17,9 +17,31 @@ class ScheduleSpec(Spec):
             s = Schedule(lessons=lessons, timezone=timezone)
 
             expect(s.timezone()).to.equal(timezone)
-    class freq_lessons(Spec):
-        def should_return_the_number_of_lessons(self):
-            pass
+    class add_schedule(Spec):
+        class when_timezones_dont_matter(Spec):
+            def should_add_schedules_directly(self):
+                timezone_1 = Timezone('Abu Dhabi')
+                lessons_1 = [{'start_time': 0, 'day_of_week': 0},
+                        {'start_time': 1, 'day_of_week': 0},
+                        {'start_time': 0.5, 'day_of_week': 0},
+                        ]
+
+                timezone_2 = Timezone('Pacific (US & Canada)')
+                lessons_2 = [{'start_time': 0, 'day_of_week': 0},
+                        {'start_time': 1, 'day_of_week': 0},
+                        {'start_time': 0.5, 'day_of_week': 0},
+                        ]
+
+                s_1 = Schedule(lessons=lessons_1, timezone=timezone_1)
+                s_2 = Schedule(lessons=lessons_2, timezone=timezone_2)
+
+                s_3 = s_1.add_schedule(s_2, timezone=None)
+
+                expect(s_3.freq_lessons_at(start_time=0, day_of_week=0)).to.equal(2)
+                expect(s_3.freq_lessons_at(start_time=1, day_of_week=0)).to.equal(2)
+                expect(s_3.freq_lessons_at(start_time=0.5, day_of_week=0)).to.equal(2)
+                expect(s_3.freq_lessons_at(start_time=23.5, day_of_week=5)).to.equal(0)
+
     class freq_lessons_at(Spec):
         def should_return_the_frequency_of_lessons_at(self):
             lessons = [{'start_time': 0, 'day_of_week': 0},
