c27c4e00267c154d9aa2e3bdfdca6b16b1168014
Edderic Ugaddan <edderic@gmail.com>
2016-09-15 15:09:16 -0400

    be able for schedule to add another to it

--- code/schedule.py
+++ code/schedule.py
@@ -7,16 +7,24 @@ class Schedule():
     QUARTERS_IN_AN_HOUR = 4
 
 
-    def __init__(self, lessons=[], timezone=None):
+    def __init__(self, lessons=[], timezone=None, table_df=pd.DataFrame()):
         self._timezone = timezone
 
-        self._table_df = pd.DataFrame(np.zeros((Schedule.NUM_START_TIMES,Schedule.NUM_DAYS)))
+        if table_df.empty:
+            self._table_df = pd.DataFrame(np.zeros((Schedule.NUM_START_TIMES, Schedule.NUM_DAYS)))
+        else:
+            self._table_df = table_df
 
         for lesson in lessons:
             self.add_lesson(lesson)
 
     def timezone(self):
         return self._timezone
+
+    def add_schedule(self, other_schedule, timezone=None):
+        new_table = self._table_df.values + other_schedule._table_df.values
+
+        return Schedule(table_df=pd.DataFrame(new_table), timezone=timezone)
 
     def freq_lessons_at(self, start_time=None, day_of_week=None):
         return self._table_df[day_of_week][self.convert_start_time_to_index(start_time)]
