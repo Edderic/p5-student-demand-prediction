ed1ab5794c62b146560107335ecbaaaf49bd0f9c
Edderic Ugaddan <edderic@gmail.com>
2016-10-06 10:11:21 -0400

    optimize interpolation between TimezoneProbModel and GeneralProbModel

--- code/models.py
+++ code/models.py
@@ -28,28 +28,81 @@ class Model(object):
 # TimezoneProbModel more. If in between,
 
 class InterpolatedProbModel(Model):
+    def __init__(self):
+        self.training_data_span_months = 15
+        self.x_offset = 139.016
+        self.x_divisor = 20
+        self.max_iter = 50
+        self.should_optimize = True
+
     def fit(self, training_data):
         # self.training_data = training_data
         self.validation = Validation(training_data)
-        self.training_data = training_data
-        self.x_offset = 80.0
-        self.x_divisor = 25.0
+        self._training_data = training_data
+
+        # Want to go for minimum mean training error * std.dev
+
+        if self.should_optimize:
+            optimized_args = self.optimize_interpolator()
+            self.x_offset = optimized_args[0]
+            self.x_divisor = optimized_args[1]
+
+        # get validation set
+        # first is x_offset, then x_divisor
+    def optimize_interpolator(self):
+
+        cons = ({'fun': lambda x: x[0], 'type': 'ineq' },
+                {'fun': lambda x: x[1] - 1, 'type': 'ineq' }) # only use low weights
+
+        val = minimize(self.evaluate_training_error,
+                [self.x_offset, self.x_divisor],
+                method='COBYLA',
+                constraints=cons,
+                options={'maxiter': self.max_iter, 'disp': True})
+
+        return val.x
+
+
+    def evaluate_training_error(self, args):
+        self.x_offset = args[0]
+        self.x_divisor = args[1]
+
+        minimizables = []
+
+        for i in range(0,self.num_months()-self.training_data_span_months):
+            training_data = self.training_data(i, self.training_data_span_months)
+            test_data = self.test_data(i+self.training_data_span_months)
+            pred = self.predict(BusinessForecast(test_data).convert(),
+                    training_data=training_data)
+            actual = ActualSchedule(test_data).bins()
+
+            to_minimize = mean_absolute_error(pred,actual) * (pred - actual).std()
+            minimizables.append(to_minimize)
+
+
+        np_to_minimize = np.array(minimizables)
+        avg = np_to_minimize.mean()
+        print "x_offset: {}, x_divisor: {}, minimize: {}".format(self.x_offset,
+                self.x_divisor,
+                avg)
+
+        return avg
+
 
     def predict(self, business_forecast, training_data=pd.DataFrame()):
         sum_bins = np.zeros(6)
 
         for i in business_forecast:
             gpm = GeneralProbModel()
-            gpm.fit(self.training_data)
+            gpm.fit(training_data)
             gpm_prediction = gpm.predict([i])
 
             tpm = TimezoneProbModel()
-            tpm.fit(self.training_data)
-            tpm_prediction = tpm.predict([i], training_data=self.training_data)
+            tpm.fit(training_data)
+            tpm_prediction = tpm.predict([i], training_data=training_data)
 
-            tmp = self.timezone_model_probability(\
-                    self.training_data[self.training_data.user_tz == i['user_tz']\
-                    ].shape[0])
+            people_at_user_tz = training_data[training_data.user_tz == i['user_tz']]
+            tmp = self.timezone_model_probability(people_at_user_tz.schedule_type.sum())
 
             sum_bins += gpm_prediction * (1 - tmp) + tpm_prediction * tmp
 
@@ -58,22 +111,32 @@ class InterpolatedProbModel(Model):
     #private
     def timezone_model_probability(self, sample_size):
         stuff = (sample_size - self.x_offset) / self.x_divisor
-        return 1.0 / (1 + math.exp(-stuff))
 
-    # def size(self):
-        # return self.validation.size()
+        print "sample_size: {}, x_offset: {}, x_divisor: {}, stuff: {}".format(sample_size,
+                self.x_offset, self.x_divisor, stuff)
 
-    # def num_months(self):
-        # return self.validation.num_months()
+        # prevent overflow error
+        if stuff < -549:
+            exponent = -549
+        else:
+            exponent = stuff
 
-    # def training_data(self, *args):
-        # return self.validation.training_data(*args)
+        return 1.0 / (1 + math.exp(-exponent))
 
-    # def year_month_index(self, *args):
-        # return self.validation.year_month_index(*args)
+    def size(self):
+        return self.validation.size()
 
-    # def test_data(self, *args):
-        # return self.validation.test_data(*args)
+    def num_months(self):
+        return self.validation.num_months()
+
+    def training_data(self, *args):
+        return self.validation.training_data(*args)
+
+    def year_month_index(self, *args):
+        return self.validation.year_month_index(*args)
+
+    def test_data(self, *args):
+        return self.validation.test_data(*args)
 
 
 class GeneralProbModel(Model):
